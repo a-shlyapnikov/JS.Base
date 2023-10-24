4034dd69921ef94de191d0851a73e3c0c55f1d0d
Alexandr <shlyapnikoff2012@gmail.com>
2023-10-24 19:27:14 +0500

    fixed hw#1

--- hw#1/1.js
+++ hw#1/1.js
@@ -14,5 +14,5 @@
 */
 
 const celsius = +prompt('Введите температуру:');
-const fahrenheit = 9/5 *  celsius + 32;
+const fahrenheit = 9 / 5 * celsius + 32;
 alert(`Цельсий: ${celsius}, Фаренгейт: ${fahrenheit}`);
